d05009420e06398ca96dec2080b2cd339f2fb2c0
elena <58104922+elenacramer@users.noreply.github.com>
2020-11-09 10:41:21 +0000

    Uploaded train.py and udacity-project.ipynb

--- train.py
+++ train.py
@@ -14,13 +14,6 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
 
 run = Run.get_context()
 
@@ -49,6 +42,7 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+    return x_df, y_df
     
 
 def main():
@@ -62,11 +56,19 @@ def main():
 
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
+    
+    path_to_csv = 'https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'
+    ds = TabularDatasetFactory.from_delimited_files(path=path_to_csv)
+    x, y = clean_data(ds)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
-    accuracy = model.score(x_test, y_test)
-    run.log("Accuracy", np.float(accuracy))
+    Accuracy = model.score(x_test, y_test)
+    run.log("Accuracy", np.float(Accuracy))
+
+    os.makedirs('outputs', exist_ok=True)
+    joblib.dump(model, 'outputs/model_hd.joblib') 
 
 if __name__ == '__main__':
     main()
